7bb571edb7da6bf5d5f5d00acb372d12f58cad01
luixlacrux <luixlacrux18@gmail.com>
2015-11-03 13:49:52 -0430

    Terminando de agregar Thumbnils

--- functions.php
+++ functions.php
@@ -2,10 +2,9 @@
 register_nav_menus( array(
 	 'menu' => "Menu Principal",
 	 ));
-?>
 
-<?php 
-add_theme_support('posts_thumbails');
-add_image_size('slide_thumbils', 470, 300, true);
-add_image_size('list_articles__sizze', 450, 370, true);
+add_theme_support( 'post-thumbnails' );
+add_image_size( 'slider_thumbs', 470, 300, true );
+add_image_size( 'list_articles_thumbs', 450, 370, true );
+
 ?>
--- index.php
+++ index.php
@@ -9,7 +9,9 @@
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<article>
 				
-				<div class="thumb"><a href="<?php the_permalink(); ?>"></a></div>
+				<div class="thumb"><a href="<?php the_permalink(); ?>">
+					<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'list_articles_thumbs' ); } ?>
+				</a></div>
 				
 				<hgroup><h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2></hgroup>
 				<div class="date"><?php the_date();?> en <span><?php the_category();?></span> </div>
